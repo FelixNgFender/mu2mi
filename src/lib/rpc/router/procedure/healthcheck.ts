0cbf068b71163258f62c45612026f4612f88b666
felixngfender <ngthinh302@gmail.com>
2025-09-12 19:10:40 -0400

    fix: accurate healthcheck metrics

--- src/lib/rpc/router/procedure/healthcheck.ts
+++ src/lib/rpc/router/procedure/healthcheck.ts
@@ -11,21 +11,14 @@ const execute = base
     const start = performance.now();
     await db.execute(sql`SELECT 1`);
     const databaseLatency = (performance.now() - start).toFixed(2);
-    const cacheLatency = (
-      performance.now() -
-      start -
-      Number(databaseLatency)
-    ).toFixed(2);
     await fileStorage.bucketExists(env.S3_BUCKET_NAME);
     const fileStorageLatency = (
       performance.now() -
       start -
-      Number(databaseLatency) -
-      Number(cacheLatency)
+      Number(databaseLatency)
     ).toFixed(2);
     return {
       databaseLatency: `${databaseLatency}ms`,
-      cacheLatency: `${cacheLatency}ms`,
       fileStorageLatency: `${fileStorageLatency}ms`,
     };
   });
