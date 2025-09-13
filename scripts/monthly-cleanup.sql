24310198453c06f88e557d06b65c9521d024d43d
felixngfender <ngthinh302@gmail.com>
2025-09-12 23:32:10 -0400

    chore: delete orphan assets on monthly cleanups

--- scripts/monthly-cleanup.sql
+++ scripts/monthly-cleanup.sql
@@ -1,6 +1,15 @@
 -- Delete assets and tracks older than 30 days
 -- Careful to sync with table names in schema
+BEGIN;
+
 DELETE FROM track
 WHERE
     updated_at < NOW() - INTERVAL '30 days'
     OR created_at < NOW() - INTERVAL '30 days';
+
+DELETE FROM asset
+WHERE
+    updated_at < NOW() - INTERVAL '30 days'
+    OR created_at < NOW() - INTERVAL '30 days';
+
+COMMIT;
