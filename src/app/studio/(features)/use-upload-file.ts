7db3282a03bfa10380b556d3525a840f1dbdfda0
felixngfender <ngthinh302@gmail.com>
2025-09-12 23:29:53 -0400

    chore: dismiss loading toast after 4s

--- src/app/studio/(features)/use-upload-file.ts
+++ src/app/studio/(features)/use-upload-file.ts
@@ -28,7 +28,7 @@ export function useUploadFile() {
   return useMutation<void, Error, UploadFileParams, unknown>({
     mutationFn: uploadFile,
     onMutate() {
-      toast.loading("Your file(s) are being uploaded.");
+      toast.loading("Your file(s) are being uploaded.", { duration: 4000 });
     },
     onSuccess() {
       toast.success("File(s) uploaded successfully!");
